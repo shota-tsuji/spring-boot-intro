809d11844e5df594a222c22db7463a5f83af7d51
shota-tsuji <shotatuzi@gmail.com>
2021-07-11 21:03:38 +0900

    feat: GETリクエストを受け取る機能を実装

--- app/src/main/java/spring/boot/hello/world/controller/HelloController.java
+++ app/src/main/java/spring/boot/hello/world/controller/HelloController.java
@@ -1,13 +1,23 @@
 package spring.boot.hello.world.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String greeting() {
+        return "hello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false) String message, Model model) {
+        model.addAttribute("sample", message);
+
         return "hello";
     }
 }
