cbc3344e1266fd329702ef9b3ada32017d8f85bb
shota-tsuji <shotatuzi@gmail.com>
2021-07-16 22:49:56 +0900

    feat: POSTリクエストを受け取る機能を実装

--- app/src/main/java/spring/boot/hello/world/controller/HelloController.java
+++ app/src/main/java/spring/boot/hello/world/controller/HelloController.java
@@ -3,6 +3,7 @@ package spring.boot.hello.world.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
 
     @GetMapping("/")
-    public String greeting() {
-        return "hello";
+    public String index() {
+        return "form";
+    }
+
+    @PostMapping("/confirm")
+    public String confirm(@RequestParam String message, Model model) {
+        model.addAttribute("message", message);
+
+        return "confirm";
     }
 
     @GetMapping("/greeting")
