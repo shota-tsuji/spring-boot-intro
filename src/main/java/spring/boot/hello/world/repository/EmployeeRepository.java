e19f749b16c0dbfc97ca077641371ed7410dff35
shota-tsuji <shotatuzi@gmail.com>
2021-08-03 08:01:46 +0900

    refactor: 不要なクラスなどを削除

--- src/main/java/spring/boot/hello/world/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package spring.boot.hello.world.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring.boot.hello.world.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
